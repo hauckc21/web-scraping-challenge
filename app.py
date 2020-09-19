ea244662d9f30c307ba946989e4498b38430b676
hauckc21 <hauck.chris@gmail.com>
2020-09-18 21:13:02 -0500

    app started

---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+app = Flask(__name__)
+
+app.config["Mongo_URI"] = "mongodb://localhost:27017/mars_app")
